b32a0bcb2b9c9ba7583df34508d239d61d1394c4
Patrick Chan <patrickchan1999@gmail.com>
2018-07-14 18:26:15 -0400

    Modify Plugin trait so that an instance does not need to be constructed

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,6 @@ pub mod wordlist_plugin;
 pub trait Plugin {
 
     fn can_handle(search_term: String) -> bool;
-    fn get_search_result(&self, search_term: String) -> Result<Vec<String>, ()>;
+    fn get_search_result(search_term: String) -> Result<Vec<String>, ()>;
     
 }

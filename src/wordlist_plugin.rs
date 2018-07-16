40bbf9b675baacc7a16db697bc1eaf225ac5f96e
Patrick Chan <patrickchan1999@gmail.com>
2018-07-15 21:44:30 -0400

    Add description method and modify get_search_result to take self

--- src/wordlist_plugin.rs
+++ src/wordlist_plugin.rs
@@ -4,6 +4,8 @@ use filters::substring_filter::SubstringFilter;
 use searchers::Search;
 use searchers::wordlist_searcher::WordlistSearcher;
 
+pub static DESCRIPTION: &'static str = "A wordlist plugin";
+
 pub struct WordlistPlugin;
 
 impl Plugin for WordlistPlugin {
@@ -12,7 +14,11 @@ impl Plugin for WordlistPlugin {
         search_term.starts_with(":wordlist ")
     }
 
-    fn get_search_result(search_term: String) -> Result<Vec<String>, ()> {
+    fn description(&self) -> &'static str {
+        DESCRIPTION
+    }
+
+    fn get_search_result(&self, search_term: String) -> Result<Vec<String>, ()> {
         if !Self::can_handle(search_term.clone()) {
             return Err(());
         } 
@@ -32,7 +38,7 @@ mod tests {
 
     #[test]
     fn simple_search() {
-        assert_eq!(WordlistPlugin::get_search_result(String::from(":wordlist sss")),
+        assert_eq!(WordlistPlugin.get_search_result(String::from(":wordlist sss")),
                    Ok(vec!["asssembler".to_string(), "bossship".to_string(), "demigoddessship".to_string(),
                            "earlesss".to_string(), "goddessship".to_string(), "headmistressship".to_string(),
                            "passsaging".to_string(), "patronessship".to_string()]));
@@ -40,13 +46,13 @@ mod tests {
 
     #[test]
     fn missing_prefix_search() {
-        assert_eq!(WordlistPlugin::get_search_result(String::from("sss")),
+        assert_eq!(WordlistPlugin.get_search_result(String::from("sss")),
                    Err(()));
     }
 
     #[test]
     fn wrong_prefix_search() {
-        assert_eq!(WordlistPlugin::get_search_result(String::from(":wrongprefix sss")),
+        assert_eq!(WordlistPlugin.get_search_result(String::from(":wrongprefix sss")),
                    Err(()));
     }
 
